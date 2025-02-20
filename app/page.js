ac7ae6ceea616085abdf8af7d7bb0646435941e1
Dimitris Kalligas <di.kalligas@gmail.com>
2025-02-20 15:50:10 +0200

    now with server action

--- app/page.js
+++ app/page.js
@@ -2,9 +2,11 @@ import Form from 'next/form'
 
 export default function Home() {
   return (
-    <Form action='/search'>
-      <input name='item' />
-      <button type='submit'>Go</button>
-    </Form>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Form className="flex flex-col gap-y-2" action={action}>
+        <input className="px-4 py-2 rounded" name='item' />
+        <button className="bg-teal-700 text-white p-1 disabled:bg-gray-500 rounded-lg" type='submit'>Go</button>
+      </Form>
+    </main>
   );
 }
